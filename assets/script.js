b5f6a8e1b82e7203b7968244e07fee4f86785e2d
Leohne <bricemagneville.pro@gmail.com>
2023-03-11 02:29:01 +0100

    ajout des évenements

--- assets/script.js
+++ assets/script.js
@@ -19,17 +19,57 @@ const slides = [
 
 const droite = document.querySelector(".arrow_right");
 const gauche = document.querySelector(".arrow_left");
+
 const nbrSlide = slides.length
 
-let i = 0;
+const imgBanner = document.querySelector(".banner-img") 
+const blaBla = document.querySelector('#banner > p')
 
-function next() {
-	for (let i = 0; slides[i] < slides.length; i++ ) {
-		console.log(i)
+let count = 0;
+	 
+	function recup(){
+		imgBanner.src = `./assets/images/slideshow/${slides[count].image}`
+		blaBla.innerHTML = `${slides[count].tagLine}`
+		console.log(blaBla.innerHTML)
+	}
+
+	function nvxDot(){
+		const dot = document.querySelector(".dots")
+		let crea = document.createElement('div')
+		dot.appendChild(crea)		
+	}
+	nvxDot()
+
+	function sndDot(){
+
+		console.log(x)		
+	} 
+	sndDot()
+
+	
+
+	function next(){ 
+	//count = count + number; /*(ou count += number)*/
+	if(count < nbrSlide -1){
+		count++;	
+	} else {
+		count = 0;
 	}
 }
 
+function before(){ 
+	if(count === 0 ){
+		count = nbrSlide -1;
+	} else {
+		count--;
+	}
+}
 
-
-droite.addEventListener("click", next)
-gauche.addEventListener("click", next)
+droite.addEventListener("click", (e) => {
+	next(), recup(), sndDot
+	console.log(count)
+})
+gauche.addEventListener("click", (e) => {
+	before(), recup()
+	console.log(count)
+})
